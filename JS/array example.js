184160f8ea572634104388fc55d10d07702e66e9
Umesh998 <kashyapumeshkumar122@gmail.com>
2024-01-06 20:33:46 +0530

    date 06-01-2023

--- JS/array example.js
+++ JS/array example.js
@@ -1,5 +1,6 @@
-//dynamic lable 
 
+
+//dynamic lable 
 function lable(ride) {
     let label = document.createElement('label');
     label.innerText = ride + '\n';
@@ -12,6 +13,7 @@ function forcarspush() {
     let cars = ["Ferarri", "Porsche", "Bentley", "Bugatti", "Ford", "Koenigsegg"];
 
     cars.push("Mercedes", "BMW");
+
     for (let ride of cars) {
         lable(ride);
     }
@@ -23,6 +25,7 @@ function forcarspop() {
     let cars = ["Ferarri", "Porsche", "Bentley", "Bugatti", "Ford", "Koenigsegg", "Mercedes", "BMW"];
 
     cars.pop("BMW");
+
     for (let ride of cars) {
         lable(ride);
     }
@@ -34,6 +37,7 @@ function forbikespush() {
     let bikes = ["Suzuki", "BMW", "Ducati", "Royalenfield", "Hero"];
 
     bikes.push("Bajaj", "Jawa");
+
     for (let ride of bikes) {
         lable(ride);
     }
@@ -45,8 +49,59 @@ function forbikespop() {
     let bikes = ["Suzuki", "BMW", "Ducati", "Royalenfield", "Hero", "Bajaj", "Jawa"];
 
     bikes.pop("Jawa");
+
     for (let ride of bikes) {
         lable(ride);
     }
 
 }
+
+// array 1 example of sort with for of
+
+function plantz() {
+    let plants = ["Tulsi", "Mogra", "Mint", "Aloevera", "Jasmine"];
+    plants.sort();
+
+    for (let ride of plants) {
+        lable(ride);
+    }
+}
+
+
+//array 1 example of reverse 
+
+function plantze() {
+    let plants = ["Tulsi", "Mogra", "Mint", "Aloevera", "Jasmine"];
+    plants.reverse();
+
+    for (let ride of plants) {
+        lable(ride);
+    }
+}
+
+//array 2 example of sort with for in 
+
+function trees(){
+    let treez = ["Banyan", "Neem", "Peepal", "Gulmohar", "Banana"];
+    
+    treez.sort();
+
+    for(let x  in treez){
+        lable(x);
+    }
+}
+
+//array 2 example of reverse with for in 
+
+function friuts(){
+    let fruitsz = ["Banana", "Guava", "Grapes", "Jamun", "Orange"];
+
+    fruitsz.reverse();
+
+    for(let y in fruitsz){
+        lable(y);
+    }
+    
+}
+
+//a
